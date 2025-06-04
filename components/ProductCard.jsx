d6e1de6f6dac879aa80706f9a3d38c1b5ec53743
Aku <BusinessIMS@hotmail.com>
2025-06-04 20:23:41 -0300

    04 06

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,69 +1,30 @@
-// src/components/ProductCard.jsx
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const ProductCard = ({ image, title, description, price, onAddToCart }) => {
+const ProductCard = ({ products, onAddToCart }) => {
   return (
-    <div className="cards-container container row gx-3">
-    <div className="card col shadow-sm" style={{ width: '18rem' }}>
-      <img src={image} className="card-img-top" alt={title} />
-      <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text text-muted">{description}</p>
-        <h6 className="text-success fw-bold mb-3">${price}</h6>
-        <button
-          className="btn btn-primary mt-auto"
-          onClick={onAddToCart}
-        >
-          Agregar al carrito 🛒
-        </button>
-      </div>
-    </div>
-    <div className="card col shadow-sm" style={{ width: '18rem' }}>
-      <img src={image} className="card-img-top" alt={title} />
-      <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text text-muted">{description}</p>
-        <h6 className="text-success fw-bold mb-3">${price}</h6>
-        <button
-          className="btn btn-primary mt-auto"
-          onClick={onAddToCart}
-        >
-          Agregar al carrito 🛒
-        </button>
-      </div>
-    </div>
-    <div className="card col shadow-sm" style={{ width: '18rem' }}>
-      <img src={image} className="card-img-top" alt={title} />
-      <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text text-muted">{description}</p>
-        <h6 className="text-success fw-bold mb-3">${price}</h6>
-        <button
-          className="btn btn-primary mt-auto"
-          onClick={onAddToCart}
-        >
-          Agregar al carrito 🛒
-        </button>
-      </div>
-    </div>
-
-    <div className="card col shadow-sm" style={{ width: '18rem' }}>
-      <img src={image} className="card-img-top" alt={title} />
-      <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text text-muted">{description}</p>
-        <h6 className="text-success fw-bold mb-3">${price}</h6>
-        <button
-          className="btn btn-primary mt-auto"
-          onClick={onAddToCart}
-        >
-          Agregar al carrito 🛒
-        </button>
-      </div>
-    </div>
-    
-    
+    <div className="cards-container">
+      {products.map((product, index) => (
+        <div className="product-card" key={index}>
+          <img
+            src={product.image}
+            className="card-img-top"
+            alt={product.title}
+          />
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title">{product.title}</h5>
+            <p className="card-text text-muted">{product.description}</p>
+            <h6 className="card-price text-success fw-bold mb-3">
+              ${product.price}
+            </h6>
+            <button
+              className="btn btn-warning mt-auto"
+              onClick={() => onAddToCart(product)}
+            >
+              Agregar al carrito 🛒
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
