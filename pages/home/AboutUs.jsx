822f737877b6e8b25ed336e9e77a1c93ae912215
Aku <BusinessIMS@hotmail.com>
2025-06-05 21:51:26 -0300

    Hola
    Hola

---
+++ pages/home/AboutUs.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Images from "../../components/Images";
+import React from "react";
+
+function AboutUs() {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
+
+export default AboutUs;
