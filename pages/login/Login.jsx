e79a3961788d62c1366a306c0451d4035a92cd95
lucas-sanmartino <lucas.sanmartino@globant.com>
2025-06-26 21:41:56 -0300

    Intento de solucionar el login (90% terminado)
    Vamos haciendo:
    gestionar imput con react con estados
    guardar esos estados on los onchange
    con el submit del formulario hacemos una request a la api solicitando el token
    
    Nos falta
    ese token guardamos en la store de redux
    COnfiguracion de store de redux esta en diapositivas detallado
    ---------------------------------------------------
    Extrapolar estas lógicas al formulario de registro

--- pages/login/Login.jsx
+++ pages/login/Login.jsx
@@ -1,40 +1,84 @@
-
-import '../../styles/Login.css';
-import Footer from '../../components/global/Footer';
-import Header from '../../components/global/Header';
-import { Link } from 'react-router-dom';
-
+import "../../styles/Login.css";
+import Footer from "../../components/global/Footer";
+import Header from "../../components/global/Header";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [token, setToken] = useState("");
+
   return (
     <>
-    <Header />
-  <div className="login-wrapper">
-      <div className="background-overlay"></div>
-      <div className="d-flex justify-content-center align-items-center login-content">
-        <div className="login-card shadow animate-fade-in">
-          <h2 className="text-center mb-4">Bienvenido</h2>
-          <form>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">Correo electrónico</label>
-              <input type="email" className="form-control input-animated" id="email" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Contraseña</label>
-              <input type="password" className="form-control input-animated" id="password" />
-            </div>
-            <button type="submit" className="btn login-boton w-100 btn-animated mt-3">
-              Iniciar sesión
-            </button>
-          </form>
-          <p className="text-center mt-4">
-            ¿Aún no estás registrado?{' '}
-            <Link to="/register" className="link-custom">Regístrate aquí</Link>
-          </p>
+      <Header />
+      <div className="login-wrapper">
+        <div className="background-overlay"></div>
+        <div className="d-flex justify-content-center align-items-center login-content">
+          <div className="login-card shadow animate-fade-in">
+            <h2 className="text-center mb-4">Bienvenido</h2>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                axios
+                  .post("/tokens", {
+                    email: inputEmail,
+                    password: inputPassword,
+                  })
+                  .then(function (response) {
+                    console.log(response);
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+              }}
+            >
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Correo electrónico
+                </label>
+                <input
+                  value={inputEmail}
+                  onChange={(e) => {
+                    setInputEmail(e.target.value);
+                  }}
+                  type="email"
+                  className="form-control input-animated"
+                  id="email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Contraseña
+                </label>
+                <input
+                  value={inputPassword}
+                  onChange={(e) => {
+                    setInputPassword(e.target.value);
+                  }}
+                  type="password"
+                  className="form-control input-animated"
+                  id="password"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn login-boton w-100 btn-animated mt-3"
+              >
+                Iniciar sesión
+              </button>
+            </form>
+            <p className="text-center mt-4">
+              ¿Aún no estás registrado?{" "}
+              <Link to="/register" className="link-custom">
+                Regístrate aquí
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
