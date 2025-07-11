fb87b13fc6226b01c466435ad2677b8b85c6bcee
lucas-sanmartino <lucas.sanmartino@globant.com>
2025-07-11 01:11:11 -0300

    Store consume productos del BE

--- pages/store/Store.jsx
+++ pages/store/Store.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/global/Header";
 import Footer from "../../components/global/Footer";
 import HomeProducts from "../../components/homeComps/HomeProducts";
 import ProductCard from "../../components/homeComps/ProductCard";
-import products from "../../products";
+import axiosInstance from "../../src/api/axiosInstance";
 
 function Store() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axiosInstance.get("/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error al cargar productos:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Header />
